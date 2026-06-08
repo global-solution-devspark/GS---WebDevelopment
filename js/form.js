66817b5b456fe27a0a0e0087f44d56a6e95e958c
Fernando Chelotti Junior <fchelottizl@outlook.com>
2026-06-08 02:25:43 -0300

    docs: starter review

--- js/form.js
+++ js/form.js
@@ -1,28 +1,30 @@
 const form = document.getElementById("contactForm");
-form.addEventListener("submit", function(event){
+
+form.addEventListener("submit", function (event) {
     event.preventDefault();
+
     const nome = document.getElementById("nome").value.trim();
     const email = document.getElementById("email").value.trim();
     const mensagem = document.getElementById("mensagem").value.trim();
 
-    if(nome === ""){
+    if (nome === "") {
         alert("Digite seu nome.");
         return;
     }
 
-    if(email === ""){
+    if (email === "") {
         alert("Digite seu e-mail.");
         return;
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if(!emailRegex.test(email)){
+    if (!emailRegex.test(email)) {
         alert("Digite um e-mail válido.");
         return;
     }
 
-    if(mensagem === ""){
+    if (mensagem === "") {
         alert("Digite sua mensagem.");
         return;
     }
--- js/slideshow.js
+++ js/slideshow.js
@@ -9,39 +9,33 @@ const bg = document.querySelector(".hero-bg");
 let indice = 0;
 
 function trocarImagem() {
-
-    // Fade para preto
     bg.style.opacity = "0";
 
     setTimeout(() => {
-
-        // Troca a imagem quando já está escura
-        bg.style.backgroundImage =
-            `linear-gradient(
+        bg.style.backgroundImage = `
+            linear-gradient(
                 to top,
-                rgba(0,0,0,.8),
-                rgba(0,0,0,.4)
+                rgba(0, 0, 0, 0.8),
+                rgba(0, 0, 0, 0.4)
             ),
-            url('${imagens[indice]}')`;
+            url('${imagens[indice]}')
+        `;
 
-        // Fade aparecendo
         bg.style.opacity = "1";
 
         indice = (indice + 1) % imagens.length;
-
     }, 1500);
 }
 
-// Primeira imagem
-bg.style.backgroundImage =
-    `linear-gradient(
+bg.style.backgroundImage = `
+    linear-gradient(
         to top,
-        rgba(0,0,0,.8),
-        rgba(0,0,0,.4)
+        rgba(0, 0, 0, 0.8),
+        rgba(0, 0, 0, 0.4)
     ),
-    url('${imagens[0]}')`;
+    url('${imagens[0]}')
+`;
 
 indice = 1;
 
-// Troca a cada 5 segundos
 setInterval(trocarImagem, 5000);
